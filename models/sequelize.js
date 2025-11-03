9e7ad8a208b54204ac5e7d4b24dafe49cefe63f6
ghl-word <ens-gabriellibmann@ugv.edu.br>
2025-11-03 20:26:04 -0300

    Delete models directory

--- models/sequelize.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("db-escola", "postgres", "postgres", {
-  host: "localhost",
-  dialect: "postgres",
-  port: 5432,
-});
-
-module.exports = sequelize;
